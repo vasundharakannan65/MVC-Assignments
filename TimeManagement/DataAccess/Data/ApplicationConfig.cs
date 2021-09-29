f11f357d86de3482ceee38cdea1d7f00ed699f55
Vasundhara S Kannan <vasundharask@gislen.com>
2021-09-29 22:19:49 +0530

    identity pending

---
+++ TimeManagement/DataAccess/Data/ApplicationConfig.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    class ApplicationConfig
+    {
+        public ApplicationConfig()
+        {
+            var configBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configBuilder.AddJsonFile(path, false);
+            var root = configBuilder.Build();
+            var appSetting = root.GetSection("ConnectionStrings:DefaultConnection");
+            SqlConnectionString = appSetting.Value;
+        }
+
+        public string SqlConnectionString { get; set; }
+    }
+}
---
+++ TimeManagement/DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+
+        public class OptionsBuild
+        {
+            public OptionsBuild()
+            {
+                Settings = new ApplicationConfig();
+                OpsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+                OpsBuilder.UseSqlServer(Settings.SqlConnectionString);
+                DbOptions = OpsBuilder.Options;
+            }
+
+            public DbContextOptionsBuilder<ApplicationDbContext> OpsBuilder { get; set; }
+            public DbContextOptions<ApplicationDbContext> DbOptions { get; set; }
+            private ApplicationConfig Settings { get; set; }
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
